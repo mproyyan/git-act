edd1d7ac5a91655f933114d81c6b1e2c70674fcc
mproyyan <mproyyan@gmail.com>
2022-12-02 17:57:45 +0700

    remove godotenv dependency

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module git-act
 
 go 1.19
-
-require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	name := os.Getenv("NAME")
 	email := os.Getenv("EMAIL")
